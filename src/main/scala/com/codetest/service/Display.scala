bb2933e345a7e003888fe5e711fcb249ca03a6be
Xiaohe Dong <danny.dong@rea-group.com>
2017-08-14 21:35:45 +1000

    Add rest of display fields for ticket , user and organization

--- src/main/scala/com/codetest/service/Display.scala
+++ src/main/scala/com/codetest/service/Display.scala
@@ -19,30 +19,66 @@ object Display {
     }
   }
 
-  def forUser(user: UserExtension): String = {
+  def forUser(userExtension: UserExtension): String = {
     s"""
-       |_id: ${user.user._id}
-       |active: ${user.user.active}
-       |email: ${user.user.email.getOrElse("")}
-       |submitted ticket number: ${user.submitTicket.length}
-       |assigned ticket number: ${user.assignTicket.length}
-       |organization name: ${user.organization.map(_.name).getOrElse("")}
+       |_id: ${userExtension.user._id}
+       |url: ${userExtension.user.url}
+       |external_id: ${userExtension.user.external_id}
+       |name: ${userExtension.user.name}
+       |created_at: ${userExtension.user.created_at}
+       |last_login_at: ${userExtension.user.last_login_at}
+       |phone: ${userExtension.user.phone}
+       |signature: ${userExtension.user.signature}
+       |tags: ${userExtension.user.tags.mkString(", ")}
+       |role: ${userExtension.user.role}
+       |active: ${userExtension.user.active}
+       |shared: ${userExtension.user.shared}
+       |suspended: ${userExtension.user.suspended}
+       |verified: ${userExtension.user.verified.getOrElse("")}
+       |alias: ${userExtension.user.alias.getOrElse("")}
+       |timezone: ${userExtension.user.timezone.getOrElse("")}
+       |organization_id: ${userExtension.user.organization_id.getOrElse("")}
+       |locale: ${userExtension.user.locale.getOrElse("")}
+       |email: ${userExtension.user.email.getOrElse("")}
+       |organization name: ${userExtension.organization.map(_.name).getOrElse("")}
+       |submit tickets number: ${userExtension.submitTicket.length}
+       |submit tickets: ${userExtension.submitTicket.map(_._id).mkString(", ")}
+       |assign tickets number: ${userExtension.assignTicket.length}
+       |assign tickets: ${userExtension.assignTicket.map(_._id).mkString(", ")}
         """.stripMargin
   }
 
   def forTicket(ticketExtension: TicketExtension): String = {
     s"""
        |_id: ${ticketExtension.ticket._id}
+       |external_id: ${ticketExtension.ticket.external_id}
+       |created_at: ${ticketExtension.ticket.created_at}
+       |subject: ${ticketExtension.ticket.subject}
+       |priority: ${ticketExtension.ticket.priority}
        |status: ${ticketExtension.ticket.status}
        |tags: ${ticketExtension.ticket.tags.mkString(", ")}
+       |has_incidents: ${ticketExtension.ticket.has_incidents}
+       |via: ${ticketExtension.ticket.via}
+       |submitter_id: ${ticketExtension.ticket.submitter_id.getOrElse("")}
+       |submitter_name: ${ticketExtension.submitter.map(_.name).getOrElse("")}
+       |organization_id: ${ticketExtension.ticket.organization_id.getOrElse("")}
+       |organization_name: ${ticketExtension.organization.map(_.name).getOrElse("")}
+       |assignee_id: ${ticketExtension.ticket.assignee_id.getOrElse("")}
+       |assignee_name: ${ticketExtension.assigner.map(_.name).getOrElse("")}
         """.stripMargin
   }
 
   def forOrganization(organization: Organization): String = {
     s"""
        |_id: ${organization._id}
+       |url: ${organization.url}
+       |external_id: ${organization.external_id}
        |name: ${organization.name}
+       |created_at: ${organization.created_at}
        |details: ${organization.details}
+       |shared_tickets: ${organization.shared_tickets}
+       |domain_names: ${organization.domain_names.mkString(", ")}
+       |tags: ${organization.tags.mkString(", ")}
         """.stripMargin
   }
 }
--- src/test/scala/com/codetest/service/DisplayTest.scala
+++ src/test/scala/com/codetest/service/DisplayTest.scala
@@ -25,11 +25,17 @@ class DisplayTest extends BaseTestSuite {
   test("should display one organization") {
     val expectedStr = s"""
                          |_id: 1
+                         |url: organization url
+                         |external_id: organization external id
                          |name: organization name
+                         |created_at: organization created_at
                          |details: organization details
+                         |shared_tickets: false
+                         |domain_names: domain name
+                         |tags: tag
        """.stripMargin
 
-    val result = Display.forOrganization(defaultOrganization)
+    val result = Display.forOrganization(defaultOrganization.copy(domain_names = List("domain name"), tags = List("tag")))
 
     result contains expectedStr should equal(true)
   }
@@ -37,15 +43,41 @@ class DisplayTest extends BaseTestSuite {
   test("should display one user") {
     val expectedStr = s"""
                          |_id: 0
+                         |url: user url
+                         |external_id: user external_id
+                         |name: user name
+                         |created_at: user created_at
+                         |last_login_at: user last_login_at
+                         |phone: phone
+                         |signature: signature
+                         |tags: tag
+                         |role: role
                          |active: false
+                         |shared: false
+                         |suspended: false
+                         |verified: true
+                         |alias: alias
+                         |timezone: timezone
+                         |organization_id: 1
+                         |locale: locale
                          |email: email
-                         |submitted ticket number: 0
-                         |assigned ticket number: 0
                          |organization name: organization
+                         |submit tickets number: 1
+                         |submit tickets: ticket id
+                         |assign tickets number: 1
+                         |assign tickets: ticket id
       """.stripMargin
 
-    val result = Display.forUser(UserExtension(defaultUser.copy(email = Some("email")),
-      Some(defaultOrganization.copy(name = "organization")), Nil, Nil))
+    val result = Display.forUser(UserExtension(defaultUser.copy(
+      email = Some("email"),
+      tags = List("tag"),
+      verified = Some(true),
+      alias = Some("alias"),
+      timezone = Some("timezone"),
+      organization_id = Some(1),
+      locale = Some("locale")
+    ),
+      Some(defaultOrganization.copy(name = "organization")), List(defaultTicket), List(defaultTicket)))
 
     result contains expectedStr should equal(true)
   }
@@ -54,12 +86,28 @@ class DisplayTest extends BaseTestSuite {
     val expectedStr =
       s"""
          |_id: ticket id
+         |external_id: ticket external_id
+         |created_at: ticket created_at
+         |subject: ticket subject
+         |priority: ticket priority
          |status: ticket status
          |tags: tag 1, tag 2, tag 3
+         |has_incidents: false
+         |via: via
+         |submitter_id: 1
+         |submitter_name: user name
+         |organization_id: 1
+         |organization_name: organization name
+         |assignee_id: 1
+         |assignee_name: user name
        """.stripMargin
 
     val result = Display.forTicket(TicketExtension(defaultTicket
-      .copy(tags = List("tag 1", "tag 2", "tag 3")), None, None, None))
+      .copy(tags = List("tag 1", "tag 2", "tag 3"),
+        submitter_id = Some(1),
+        assignee_id = Some(1),
+        organization_id = Some(1)
+      ), Some(defaultUser), Some(defaultUser), Some(defaultOrganization)))
 
     result contains expectedStr should equal(true)
   }
